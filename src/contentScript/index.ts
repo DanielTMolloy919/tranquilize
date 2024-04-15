6a19cc8f9fe2c05e6d1ec2654022fb51bf65cbb2
Daniel Molloy <danieltmolloy919@gmail.com>
2024-04-15 23:07:58 +1000

    Hide reddit main page

--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,1 +1,12 @@
-console.info('contentScript is running')
+chrome.runtime.onMessage.addListener((message, sender, response) => {
+  if (message.type === 'REDDIT') {
+    hideReddit();
+  }
+})
+
+function hideReddit() {
+  const subgrid = document.getElementsByClassName('subgrid-container')
+  if (subgrid.length) {
+    subgrid[0].style.display = 'none'
+  }
+}
