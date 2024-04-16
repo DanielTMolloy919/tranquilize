177362a750372eb307e209bfb67b269df48cd143
Daniel Molloy <danieltmolloy919@gmail.com>
2024-04-16 18:04:45 +1000

    Working shadcnui switch

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
+import React from 'react'
+import { Switch } from '@pages/popup/components/ui/switch'
+import { Label } from '@pages/popup/components/ui/label'
 
 export default function Popup(): JSX.Element {
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
-      <header className="flex flex-col items-center justify-center text-white">
-        <img src={logo} className="h-36 pointer-events-none animate-spin-slow" alt="logo" />
-        <p>
-          Edit <code>src/pages/popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="text-blue-400"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <p className="bg-pink-400">Popup styled with TailwindCSS!</p>
-      </header>
+      <div className="flex items-center space-x-2">
+        <Switch id="airplane-mode" />
+        <Label htmlFor="airplane-mode">Airplane Mode</Label>
+      </div>
     </div>
-  );
+  )
 }
