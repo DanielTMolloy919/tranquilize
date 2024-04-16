69d59fe1a4c7926a7cf52f882464de67c3e70983
Daniel Molloy <danieltmolloy919@gmail.com>
2024-04-16 18:14:07 +1000

    Home feed switch ui

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -4,10 +4,10 @@ import { Label } from '@pages/popup/components/ui/label'
 
 export default function Popup(): JSX.Element {
   return (
-    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 bg-gray-800">
+    <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3">
       <div className="flex items-center space-x-2">
-        <Switch id="airplane-mode" />
-        <Label htmlFor="airplane-mode">Airplane Mode</Label>
+        <Switch id="reddit-home-feed" />
+        <Label htmlFor="reddit-home-feed">Hide Home Feed</Label>
       </div>
     </div>
   )
