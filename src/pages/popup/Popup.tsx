d7c5a0cc31aff1236beefa82c965c0ac69363a36
Daniel Molloy <danieltmolloy919@gmail.com>
2024-04-28 14:17:14 +1000

    refactor: popup code

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -3,6 +3,21 @@ import { Switch } from "@pages/popup/components/ui/switch";
 import { Label } from "@pages/popup/components/ui/label";
 import { defaultSettings, Settings } from "@pages/popup/lib/types";
 
+const settingsList = [
+  { id: "reddit-home-feed", label: "Hide All Feeds", key: "hideHomeFeed" },
+  { id: "reddit-sidebar", label: "Hide Sidebar", key: "hideSidebar" },
+  {
+    id: "reddit-suggestions",
+    label: "Hide Suggested Posts",
+    key: "hideSuggestions",
+  },
+  {
+    id: "reddit-trending-searches",
+    label: "Hide Trending Searches",
+    key: "hideTrendingSearches",
+  },
+];
+
 export default function Popup() {
   const [settings, setSettings] = useState<Settings | null>(null);
 
@@ -29,49 +44,18 @@ export default function Popup() {
 
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 text-center h-full p-3 flex flex-col gap-2">
-      <div className="flex items-center space-x-2">
-        <Switch
-          id="reddit-home-feed"
-          checked={settings.hideHomeFeed}
-          onCheckedChange={(checked) => {
-            setSettings({ ...settings, hideHomeFeed: checked });
-          }}
-        />
-        <Label htmlFor="reddit-home-feed">Hide Home Feed</Label>
-      </div>
-
-      <div className="flex items-center space-x-2">
-        <Switch
-          id="reddit-sidebar"
-          checked={settings.hideSidebar}
-          onCheckedChange={(checked) => {
-            setSettings({ ...settings, hideSidebar: checked });
-          }}
-        />
-        <Label htmlFor="reddit-sidebar">Hide Sidebar</Label>
-      </div>
-
-      <div className="flex items-center space-x-2">
-        <Switch
-          id="reddit-suggestions"
-          checked={settings.hideSuggestions}
-          onCheckedChange={(checked) => {
-            setSettings({ ...settings, hideSuggestions: checked });
-          }}
-        />
-        <Label htmlFor="reddit-suggestions">Hide Suggestions</Label>
-      </div>
-
-      <div className="flex items-center space-x-2">
-        <Switch
-          id="reddit-trending-searches"
-          checked={settings.hideTrendingSearches}
-          onCheckedChange={(checked) => {
-            setSettings({ ...settings, hideTrendingSearches: checked });
-          }}
-        />
-        <Label htmlFor="reddit-trending-searches">Trending Searches</Label>
-      </div>
+      {settingsList.map(({ id, label, key }) => (
+        <div key={id} className="flex items-center space-x-2">
+          <Switch
+            id={id}
+            checked={settings[key]}
+            onCheckedChange={(checked) => {
+              setSettings({ ...settings, [key]: checked });
+            }}
+          />
+          <Label htmlFor={id}>{label}</Label>
+        </div>
+      ))}
     </div>
   );
 }
