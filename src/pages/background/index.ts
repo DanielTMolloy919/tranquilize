d271ac24c2ac73a6dc76843969215a4c253032f2
Daniel Molloy <danieltmolloy919@gmail.com>
2024-05-07 21:18:03 +1000

    feat: update icon

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,10 +1,9 @@
-import {defaultSettings} from "@pages/popup/lib/types";
+import { defaultSettings } from "@pages/popup/lib/types";
 
 chrome.runtime.onInstalled.addListener(() => {
-        chrome.storage.sync.get("settings", (data) => {
-            if (!data.settings) {
-                chrome.storage.sync.set({ settings: defaultSettings });
-            }
-        });
+  chrome.storage.sync.get("settings", (data) => {
+    if (!data.settings) {
+      chrome.storage.sync.set({ settings: defaultSettings });
     }
-)
+  });
+});
